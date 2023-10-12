423efabd84a8c6566acdc82c52054d205bd1ad10
EduardoBrandao <eduardo.loureiro@madeinweb.app>
2023-10-11 23:12:12 -0300

    finalizando a função de monitoramento e implementando a função de imprimir logs

--- hello.go
+++ hello.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 ) 
 
-const monitoramentos = 5
+const monitoramentos = 2
 const delay = 2
 
 func main(){
@@ -21,6 +26,7 @@ for {
 		inciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo Logs...")
+		imprimeLogs()
 	case 0: 
 		fmt.Println("Saindo do Prorama...")
 		os.Exit(0)
@@ -54,7 +60,8 @@ func exibeMenu() {
 
 func inciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	sites := []string{"https://www.alura.com.br", "https://ramdom-status-code.herokuapp.com/", "https://www.caelum.com.br"}
+
+	sites := leSitesDoArquivo()
 
 	for i := 0; i < monitoramentos; i++ {
 
@@ -68,11 +75,66 @@ func inciarMonitoramento() {
 }
 
 func testaSite(site string){
-	resp, _ := http.Get(site)
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	}
 	
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi careagado com sucesso !!!")
+		registaLog(site, true)
 	}else {
 		fmt.Println("Site:", site, "esta com problema. Status Code:", resp.StatusCode)
+		registaLog(site, false)
 	}
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+	leitor := bufio.NewReader(arquivo)
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		fmt.Println(linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+	return sites
+
+}
+
+func registaLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR | os.O_CREATE| os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string (arquivo))
 }
